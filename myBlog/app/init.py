c4837f380fe6201b823f492ccea11befde376ee3
tuzhenyu <1572629866@qq.com>
2016-11-26 16:16:11 +0800

    完成了flask框架移植

---
+++ myBlog/app/init.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import sqlite3
+import form
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from form import LoginForm
+
+# 配置文件
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app=Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html",name='tuzhenyu')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=9998, use_reloader=True)
